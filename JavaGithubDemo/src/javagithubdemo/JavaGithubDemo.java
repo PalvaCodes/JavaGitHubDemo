9904107828f0d7f85e2cc4ced4aec7aeafe9f761
Pedro Alvarado <198825316+PalvaCodes@users.noreply.github.com>
2025-05-20 15:45:26 -0400

    Added hello world 1

--- JavaGithubDemo/src/javagithubdemo/JavaGithubDemo.java
+++ JavaGithubDemo/src/javagithubdemo/JavaGithubDemo.java
@@ -16,6 +16,7 @@ public class JavaGithubDemo {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello Github!");
+        System.out.println("Hello Github!");
     }
     
 }
